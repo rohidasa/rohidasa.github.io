d319b826ed5c46d54fa6c2beb82ae01f8bc045e7
rohidasa <rohidasa.rd@gmail.com>
2026-05-07 11:36:10 +0530

    update

--- assets/LogoutPage-C9PP2pGh.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-cxGRPVnk.js";import{Z as t}from"./client-ISF5DPme.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
---
+++ assets/LogoutPage-DOJ8XXgP.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DwZO8htP.js";import{Z as t}from"./client-BV7RdAHY.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
