ce25334a0de25741a73a569d9c8d9a23e1700397
rohidasa <rohidasa.rd@gmail.com>
2026-05-07 09:29:34 +0530

    Profile Page

---
+++ assets/LogoutPage-C9PP2pGh.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-cxGRPVnk.js";import{Z as t}from"./client-ISF5DPme.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
