932ff72a1b293e6109f8dec7ddb8d2fb828d1fba
rohidasa <rohidasa.rd@gmail.com>
2026-05-07 12:09:41 +0530

    Update

---
+++ assets/LogoutPage-ChDkejeI.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-B66v45Sj.js";import{Z as t}from"./client-CwuAYi2m.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
--- assets/LogoutPage-DOJ8XXgP.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-DwZO8htP.js";import{Z as t}from"./client-BV7RdAHY.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
