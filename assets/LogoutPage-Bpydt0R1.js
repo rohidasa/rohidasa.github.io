c6e46a6a26eef8dd73040791376008a2ae31d3bc
rohidasa <rohidasa.rd@gmail.com>
2026-05-08 18:12:20 +0530

    s

--- assets/LogoutPage-Bpydt0R1.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-BM9gC2F4.js";import{Z as t}from"./client-Bw7ZDrMT.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
