f81fec891bd0f32b6dc62ea0534b0b1e8678b517
rohidasa <rohidasa.rd@gmail.com>
2026-05-08 14:57:27 +0530

    Added new items to the gallery, text content update

--- assets/LogoutPage-B0mDgia1.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-B_tvxRKT.js";import{Z as t}from"./client-DJHnXOpM.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
---
+++ assets/LogoutPage-D0PCEUxs.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-Ds94CbYS.js";import{Z as t}from"./client-C2TPtBma.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
