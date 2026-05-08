543d541837916055a54100686b8ed06da9e6d2b3
rohidasa <rohidasa.rd@gmail.com>
2026-05-08 21:12:04 +0530

    s

---
+++ assets/LogoutPage-DEF_eo9G.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-X-wQ1aZU.js";import{Z as t}from"./client-CvbQiGjA.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
