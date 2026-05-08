7e58cb2c45264d1a58596f4a54d4d73044165584
rohidasa <rohidasa.rd@gmail.com>
2026-05-08 15:23:18 +0530

    s

---
+++ assets/LogoutPage-Bpydt0R1.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-BM9gC2F4.js";import{Z as t}from"./client-Bw7ZDrMT.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
--- assets/LogoutPage-CHoIfVBi.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-CwRIGVLR.js";import{Z as t}from"./client-CiodfZzA.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
