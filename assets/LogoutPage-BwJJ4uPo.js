a7144537afdee04a3154be21afada2de996ce542
rohidasa <rohidasa.rd@gmail.com>
2026-05-09 18:21:02 +0530

    gallery update

---
+++ assets/LogoutPage-BwJJ4uPo.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-Ck-U3xkk.js";import{Z as t}from"./client-awSLF6X_.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
--- assets/LogoutPage-DEF_eo9G.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-X-wQ1aZU.js";import{Z as t}from"./client-CvbQiGjA.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
