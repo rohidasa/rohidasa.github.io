1d1aff9c174f09e8378af4aec0c1af1fb66eac7b
rohidasa <rohidasa.rd@gmail.com>
2026-05-12 13:00:46 +0530

    s

--- assets/LogoutPage-DACXUoos.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-DWL6PqDf.js";import{Z as t}from"./client-ORJI0WKm.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
--- assets/createLucideIcon-DLAAktRR.js
+++ assets/createLucideIcon-DLAAktRR.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-DWL6PqDf.js";/**
+import{r as s}from"./index-D7J-ejss.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
