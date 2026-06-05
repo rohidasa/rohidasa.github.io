9c4f8f91291eb50f27e791c70fbc7fa8fbe26ab7
rohidasa <rohidasa.rd@gmail.com>
2026-06-05 14:40:07 +0530

    comapring with old website

---
+++ old/rohidasa.github.io/assets/LogoutPage-PYAmZ-cO.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-D7J-ejss.js";import{Z as t}from"./client-DAHo-loQ.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
