a9151847f58c1ee50eb76efc8626aa6a5de700bb
rohidasa <rohidasa.rd@gmail.com>
2026-06-05 16:44:34 +0530

    s

--- old/assets/LogoutPage-PYAmZ-cO.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-D7J-ejss.js";import{Z as t}from"./client-DAHo-loQ.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
