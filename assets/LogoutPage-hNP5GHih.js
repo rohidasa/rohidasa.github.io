6a1ef87f7ccf9a8959794284b7c34b0cacf4a09c
rohidasa <rohidasa.rd@gmail.com>
2026-06-25 15:01:43 +0530

    email update

---
+++ assets/LogoutPage-hNP5GHih.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DrzbV-Hv.js";import{Z as t}from"./client-euaOqF1q.js";function n(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{n as default};
